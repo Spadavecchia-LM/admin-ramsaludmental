21f1d4bd35c6a185e045adaa5376f4be5476d005
Spadavecchia-LM <leonardo.spadavecchia@hotmail.com>
2024-02-04 18:46:01 -0300

    bug del token corregido?

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import LoginForm from "./Components/Login/LoginForm";
 import ProfesionalsTable from "./Components/Dashboard/ProfesionalsTable";
 import Unauthorized from "./Components/Error/Unauthorized";
 import { useState, useEffect, useContext } from "react";
-import FirebaseContext from "./Context/FirebaseContext";
+import { AuthContext } from "./Context/FirebaseContext";
 
 function App() {
   const [isTokenValid, setTokenValid] = useState(false);
-  const {userIsLogged} = useContext(FirebaseContext)
-  const [tokenIsValidating, setTokenIsValidating] = useState(true)
+  const {value} = useContext(AuthContext)
+  const {userIsLogged} = value
+  
   useEffect(() => {
     const validateToken = async () => {
       const token = localStorage.getItem("accessToken");
 
       if (token) {
         setTokenValid(true);
-        setTokenIsValidating(false)
+
       }
     };
 
@@ -29,7 +30,7 @@ function App() {
       <Routes>
         <Route
           path="/dashboard"
-          element={isTokenValid && !tokenIsValidating && userIsLogged ? <ProfesionalsTable /> : <Unauthorized />}
+          element={(isTokenValid && userIsLogged )? <ProfesionalsTable /> : <Unauthorized />}
         />
         <Route path="/" element={<LoginForm />} />
    
